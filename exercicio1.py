aa9fc3ea6c63bae906b91b0d03831b4ec454dea3
giovanna280808 <giovanna.correa.souza@escola.pr.gov.br>
2025-07-07 11:32:58 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,6 @@
+Aluno = ("João", 20, "2023001")
+
+nome, idade, matricula = Aluno
+print("Nome:", nome)
+print("Idade:", idade)
+print("Matrícula:", matricula)
---
+++ exercicio2.py
@@ -0,0 +1,5 @@
+def criar_estudante(nome, idade, curso):
+        return (nome, idade, curso)
+
+estudante = criar_estudante("Ana", 21, "Engenharia")
+print(estudante) #Saida: ( 'Ana', 21, 'Engenharia')
